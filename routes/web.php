1f44907aaf319e6e7829283315e10d182e3091bc
IliasKhms <152298532+IliasKhms@users.noreply.github.com>
2024-02-20 18:47:33 +0000

    test

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+use App\Http\Controllers\HomeController;
+Route::get('/', [HomeController::class, 'index']);
+
