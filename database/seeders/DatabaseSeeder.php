c0e9c78462d56e35dbd4104f4292e489405afc2d
Peti6inge <158772591+Peti6inge@users.noreply.github.com>
2024-03-06 14:46:54 +0000

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,9 +12,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        \App\Models\Recipe::factory()
+    ->count(5)
+    ->for(\App\Models\User::factory()->create())
+    ->create();
+
+
+\App\Models\Recipe::factory()
+    ->count(5)
+    ->for(\App\Models\User::factory()->create())
+    ->create();
     }
 }
