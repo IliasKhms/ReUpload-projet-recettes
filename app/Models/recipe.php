f776be887736c130a2e03e9ea676cd27e239bab5
IliasKhms <152298532+IliasKhms@users.noreply.github.com>
2024-03-20 14:38:30 +0100

    Recipe

--- app/Models/recipe.php
+++ app/Models/recipe.php
@@ -5,7 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class recipe extends Model
+class Recipe extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the user that owns the recipe.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class,'owner_id');
+    }
+
 }
