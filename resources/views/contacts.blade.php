b00729191048cecfae48dac4e302b0e28b8da7a9
IliasKhms <152298532+IliasKhms@users.noreply.github.com>
2024-03-25 14:26:38 +0100

    CSS du formulaire

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -3,15 +3,19 @@
 @section('content')
     <form action="contact.php" class="form">
         <label for="firstname">
-            <input type="text" name="firstname" id="firstname" placeholder="NOM">
+            <input type="text" name="firstname" id="firstname" placeholder="Nom">
+        </label>
+
+        <label for="lastname">
+            <input type="text" name="lastname" id="lastname" placeholder="Prénom">
         </label>
 
         <label for="email">
-            <input type="email" name="email" id="email" placeholder="EMAIL">
+            <input type="email" name="email" id="email" placeholder="Email">
         </label>
 
-        <label for="password">
-            <input type="text" name="password" id="password" placeholder="MOT DE PASSE">
+        <label for="message">
+            <textarea type="text" name="message" id="message" placeholder="Veuillez saisir votre message"></textarea>
         </label>
 
         <input type="submit" class="btn" value="Envoyer">
